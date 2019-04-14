3078bb48e500bba35756eda6a0b53163a1ef61c3
DangXuanHoaiBao <hoaibaodang1997@gmail.com>
2019-04-15 00:45:29 +0700

    add fonts file and delete webfonts file

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(express.static(path.join(__dirname, 'public/images/electronics')));
 app.use(express.static(path.join(__dirname, 'public/images/view-slider/larger')));
 app.use(express.static(path.join(__dirname, 'public/images/view-slider/medium')));
 app.use(express.static(path.join(__dirname, 'public/images/view-slider/thumbnail')));
-app.use(express.static(path.join(__dirname, 'public/webfonts'))); 
+app.use(express.static(path.join(__dirname, 'webfonts/fa-solid-900.woff2'))); 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
