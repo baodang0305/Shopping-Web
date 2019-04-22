88453d18018598d6dbce70ff12b36aea44d0f115
Spiritofthecore <cuongdepgiai1998@gmail.com>
2019-04-22 07:47:10 +0700

    add sign up tab

---
+++ public/javascripts/tabBar.js
@@ -0,0 +1,13 @@
+function changeTab(evt, act) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("nav-link");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(act).style.display = "inline-block";
+  evt.currentTarget.className += " active";
+}
