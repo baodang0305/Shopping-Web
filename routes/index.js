fdde94b68c4502713596ce34a13e92528180b4c3
HoangCuong9331 <HoangCuong9331@gmail.com>
2019-04-22 20:52:59 +0700

    add forgot pass feature

--- routes/index.js
+++ routes/index.js
@@ -13,4 +13,12 @@ router.get('/all', function(req, res,next){
 router.get('/login', function(req, res,next){
   res.render('login', { title: 'Express' });
 });
+
+router.get('/forgotPassword', function(req, res,next){
+  res.render('Repass', { title: 'Express' });
+});
+
+router.get('/account', function(req, res,next){
+  res.render('account', { title: 'Express' });
+});
 module.exports = router;
