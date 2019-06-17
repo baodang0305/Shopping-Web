ab44243053e5b6fa543544fbab40fbdf60d30962
Spiritofthecore <cuongdepgiai1998@gmail.com>
2019-06-17 14:01:43 +0700

    sửa tabbar để dẫn vào trang all-product và sửa phần comment trong index

--- controllers/product/all-product.js
+++ controllers/product/all-product.js
@@ -2,29 +2,18 @@ const express = require('express');
 const product = require('../../models/product');
 const router = express.Router();
 
-router.get('/all-product-:id', function(req, res, next){
-  var id = req.params.id;
+router.get('/all-product', function(req, res, next){
   (async()=>{
     var list_all_product;
-    if(id == 'man'){
-      list_all_product = await product.find({Gender: 'Man'});
-    }
-    if(id == 'women'){
-      list_all_product = await product.find({Gender: 'Women'});
-    }
-    if(id == 'all'){
+    console.log(req.query)
+    if (req.query.all == null) {
+      list_all_product = await product.find(req.query);
+      console.log(list_all_product)
+    } else {
       list_all_product = await product.find({});
     }
-    if(id == 'popular'){
-      list_all_product = await product.find({Product_Group: 'Popular'});
-    }
-    if(id == 'feature'){
-      list_all_product = await product.find({Product_Group: 'Feature'});
-    }
-    if(id == 'new'){
-      list_all_product = await product.find({Product_Group: 'New'});
-    }
-    res.render('all-product', { title: 'All Product Women', isLogin: Boolean(req.user), user: req.user, 'list_all_product': list_all_product});
+
+    res.render('all-product', { title: 'Danh sách sản phẩm', isLogin: Boolean(req.user), user: req.user, 'list_all_product': list_all_product});
   })();
 });
 
