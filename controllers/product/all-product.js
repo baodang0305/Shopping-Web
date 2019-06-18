23948b6389de027d3cee92f55924c583915912eb
Spiritofthecore <cuongdepgiai1998@gmail.com>
2019-06-18 13:06:10 +0700

    merge with master

--- controllers/product/all-product.js
+++ controllers/product/all-product.js
@@ -3,20 +3,22 @@ const product = require('../../models/product');
 const router = express.Router();
 
 const LIMIT = 8;
-router.get('/all-product-:id/viewpage=:page', function(req, res, next){
-  var id = req.params.id;
+router.get('/all-product/viewpage=:page', function(req, res, next){
+  var id = req.params.id
+
   var page = req.params.page;
   console.log(page);
   (async()=>{
     var list_all_product;
     if (req.query.all != null) {
-      list_all_product = await product.find({});
+      list_all_product = await product.find({}).limit(LIMIT).skip(page*LIMIT - LIMIT);
+      searchObjectString = "?all=all";
     } else {
       var searchObject = new Object()
-      if (req.query.Gender != null) {
+      if (req.query.Gender != null && req.query.Gender != "undefined") {
         searchObject.Gender = req.query.Gender
       }
-      if (req.query.Price != 0) {
+      if (req.query.Price != 0 && req.query.Price != "undefined") {
         let priceTag = req.query.Price
         switch (priceTag) {
           case 1:
@@ -43,15 +45,22 @@ router.get('/all-product-:id/viewpage=:page', function(req, res, next){
           break;
         }
       }
-      if (req.query.Category != 0) {
+      if (req.query.Category != 0 && req.query.Category != "undefined") {
         searchObject.Category = req.query.Category
       }
-      if (req.query.Product_Group != 0) {
+      if (req.query.Product_Group != 0 && req.query.Product_Group != "undefined") {
         searchObject.Product_Group = req.query.Product_Group
       }
       list_all_product = await product.find(searchObject).limit(LIMIT).skip(page*LIMIT - LIMIT);
+      searchObjectString = "?Gender=" + String(searchObject.Gender) + "&Price=" + String(searchObject.Price) + "&Category=" + String(searchObject.Category) + "&Product_Group=" + String(searchObject.ProductGroup)
     }
-    res.render('all-product', { title: 'Danh sách sản phẩm', isLogin: Boolean(req.user), user: req.user, 'list_all_product': list_all_product});
+    let count = 0
+    list_all_product.forEach(element => {
+      count += 1;
+    });
+    console.log("count" + count)
+    console.log(searchObjectString)
+    res.render('all-product', { title: 'Danh sách sản phẩm', isLogin: Boolean(req.user), user: req.user, 'list_all_product': list_all_product, page, searchObjectString, count});
   })();
 });
 
